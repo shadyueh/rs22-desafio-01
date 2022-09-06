7d7b7a9d6c76d4970faec196e8cfdadd068ba4c7
Held Grijo <held.grijo@gmail.com>
2022-09-06 11:57:30 -0400

    style: adjust line heights

--- src/components/TaskInfo.tsx
+++ src/components/TaskInfo.tsx
@@ -3,7 +3,7 @@ import styles from "./TaskInfo.module.css";
 export function TaskInfo() {
   return (
     <div className={styles.taskInfo}>
-      <p>Tarefas Criadas <span>0</span></p>
+      <p>Tarefas criadas <span>0</span></p>
       <p>Concluídas <span>0</span></p>
     </div>
   );
