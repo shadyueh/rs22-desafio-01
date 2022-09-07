1c10ed251c363c278e4d2f88c277936272515cd8
Held Grijo <held.grijo@gmail.com>
2022-09-07 18:51:10 -0400

    refactor: remove unnecessary references

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,7 @@ import "./global.css";
 
 const emptyTaskList: TaskProps[] = [];
 
-const data: TaskProps[] = [
-  {
-    id: "1",
-    content:
-      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-  },
-  {
-    id: "2",
-    content:
-      "Nulla semper vehicula tortor non ultricies. Donec ac quam ante, in feugiat sapien. Etiam massa turpis, laoreet et auctor sed, dictum sed urna. Fusce lorem ipsum, semper sit amet laoreet ac, aliquam at purus. ",
-  },
-];
-
 function App() {
-  const [countCreatedTasks, setCountCreatedTasks] = useState(0);
-  const [countFinishedTasks, setCountFinishedTasks] = useState(0);
   const [tasks, setTasks] = useState(emptyTaskList);
 
   function handleCreateNewTask(taskText: string) {
