3660294e563663de850fe1d214af6b3eca341753
Held Grijo <held.grijo@gmail.com>
2022-09-07 18:36:00 -0400

    feat: validate and add a new task

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,33 @@ import "./global.css";
 
 const emptyTaskList: TaskProps[] = [];
 
-const data: TaskProps[] =  [
-    {
-      id: "1",
-      content:
-        "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    },
-    {
-      id: "2",
-      content:
-        "Nulla semper vehicula tortor non ultricies. Donec ac quam ante, in feugiat sapien. Etiam massa turpis, laoreet et auctor sed, dictum sed urna. Fusce lorem ipsum, semper sit amet laoreet ac, aliquam at purus. ",
-    },
-  ];
+const data: TaskProps[] = [
+  {
+    id: "1",
+    content:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+  },
+  {
+    id: "2",
+    content:
+      "Nulla semper vehicula tortor non ultricies. Donec ac quam ante, in feugiat sapien. Etiam massa turpis, laoreet et auctor sed, dictum sed urna. Fusce lorem ipsum, semper sit amet laoreet ac, aliquam at purus. ",
+  },
+];
 
 function App() {
   const [countCreatedTasks, setCountCreatedTasks] = useState(0);
   const [countFinishedTasks, setCountFinishedTasks] = useState(0);
   const [tasks, setTasks] = useState(emptyTaskList);
-  const [newTaskText, setNewTaskText] = useState("");
+
+  function handleCreateNewTask(taskText: string) {
+    setTasks([...tasks, { id: tasks.length.toString(), content: taskText }]);
+  }
 
   return (
     <div>
       <Header />
-      <NewTaskForm />
-      <TaskList items={data}/>
+      <NewTaskForm onTaskCreate={handleCreateNewTask} />
+      <TaskList items={tasks} />
     </div>
   );
 }
--- src/components/NewTaskForm.tsx
+++ src/components/NewTaskForm.tsx
@@ -1,11 +1,49 @@
 import styles from "./NewTaskForm.module.css";
 import plusIcon from "../assets/plus-icon.svg";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
 
-export function NewTaskForm() {
+interface NewTaskFormProps {
+  onTaskCreate: (taskText: string) => void;
+}
+
+export function NewTaskForm({ onTaskCreate }: NewTaskFormProps) {
+  const [newTaskText, setNewTaskText] = useState("");
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+    onTaskCreate(newTaskText);
+    setNewTaskText("");
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("");
+    setNewTaskText(event.target.value);
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+    let message = "";
+    if (event.target.validity.patternMismatch)
+      message = "Não é permitido apenas espaços brancos";
+    if (event.target.validity.valueMissing)
+      message = "Esse campo é obrigatório";
+    event.target.setCustomValidity(message);
+  }
+
   return (
-    <form className={styles.newTask} action="">
-      <input type="text" name="" placeholder="Adicione uma nova tarefa" />
-      <button type="submit">Criar <img src={plusIcon} alt="" /></button>
+    <form className={styles.newTask} onSubmit={handleCreateNewTask}>
+      <input
+        type="text"
+        name="task"
+        placeholder="Adicione uma nova tarefa"
+        value={newTaskText}
+        onChange={handleNewTaskChange}
+        onInvalid={handleNewTaskInvalid}
+        pattern="(.|\s)*\S(.|\s)*"
+        required
+      />
+      <button type="submit">
+        Criar <img src={plusIcon} alt="" />
+      </button>
     </form>
   );
 }
