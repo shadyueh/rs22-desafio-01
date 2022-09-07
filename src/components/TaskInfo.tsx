a6adeb5eee64c864c12f02bc267060ff94f827ea
Held Grijo <held.grijo@gmail.com>
2022-09-07 16:07:36 -0400

    feat: count created tasks

--- src/components/TaskInfo.tsx
+++ src/components/TaskInfo.tsx
@@ -1,10 +1,15 @@
 import styles from "./TaskInfo.module.css";
 
-export function TaskInfo() {
+interface TaskInfo {
+  created: number;
+  done: number;
+}
+
+export function TaskInfo({created, done}: TaskInfo) {
   return (
     <div className={styles.taskInfo}>
-      <p>Tarefas criadas <span>0</span></p>
-      <p>Concluídas <span>0</span></p>
+      <p>Tarefas criadas <span>{created}</span></p>
+      <p>Concluídas <span>{done}</span></p>
     </div>
   );
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -5,14 +5,16 @@ import { NoTask } from "./NoTask";
 import styles from "./TaskList.module.css";
 
 interface TaskListProps {
-  items: TaskProps[];
+  items?: TaskProps[];
 }
 
 export function TaskList({ items }: TaskListProps) {
+  const hasTasks = Array.isArray(items) && items.length > 0;
+
   return (
     <div className={styles.tasks}>
-      <TaskInfo />
-      {Array.isArray(items) && items?.length > 0 ? (
+      <TaskInfo created={hasTasks ? items.length : 0} done={0} />
+      {hasTasks ? (
         items.map((task) => {
           return <Task key={task.id} id={task.id} content={task.content} />;
         })
